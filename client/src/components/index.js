6e43988955746235b353959728183aa23f47e694
asternaut <snooks.sheild@gmail.com>
2017-05-11 20:53:53 -0600

    about page working

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export {default as Home} from './Home/Home'
+export {default as About} from './About/About'
 // export component from file as 'Home'
--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 
 import App from './App';
-import {HomeContainer} from './containers'
+import {HomeContainer, AboutContainer} from './containers'
+//import {About} from './containers'
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}>
       <Route path="/home" component={HomeContainer} />
       <IndexRoute component={HomeContainer} />
+      <Route path="/about" component={AboutContainer} />
     </Route>
   </Router>,
   document.getElementById('root')
